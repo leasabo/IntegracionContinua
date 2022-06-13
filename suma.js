ab473f54a59bf38ec6455bcf2d44ca91b6f773b6
leasabo <email_id>
2022-06-13 16:06:03 -0300

    arreglando lo de eslint

--- suma.js
+++ suma.js
@@ -1,5 +1,4 @@
-function suma(a, b) { 
-  console.log(a);
+function suma(a, b) {
   return a + b;
 }
 
--- suma.test.js
+++ suma.test.js
@@ -1,10 +1,11 @@
 const suma = require('./suma');
 
-test('2 + 5 = 7', () =>{
-    expect(suma(2,5)).toBe(7);
+let test;
+let expect;
+test('2 + 5 = 7', () => {
+  expect(suma(2, 5)).toBe(7);
 });
 
-test('a + 5 = NaN', () =>{
-    expect(suma('a',5)).toBe(NaN);
+test('a + 5 = NaN', () => {
+  expect(suma('a', 5)).toBe(NaN);
 });
-
